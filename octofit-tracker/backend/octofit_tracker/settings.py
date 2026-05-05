76f4e583b3d42891a554f8ff5d414ed1ac1e3d89
AlePanaStrabag <alessandro.panarese@partner.strabag.com>
2026-05-05 09:47:12 +0000

    Aggiorna le impostazioni di ALLOWED_HOSTS e aggiungi la vista api_root per la gestione degli URL dinamici

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-yikg36wt888bbw69u3(p0^k$)!#q8*utgbsxws64rmnoael6z3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if os.environ.get('CODESPACE_NAME'):
+    ALLOWED_HOSTS.append(f"{os.environ.get('CODESPACE_NAME')}-8000.app.github.dev")
 
 
 # Application definition
